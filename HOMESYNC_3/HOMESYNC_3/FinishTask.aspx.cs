8549dd727f2aae334b1880b44157a53565470de9
adhambarakatco <adham.hisham@student.giu-uni.de>
2024-11-15 21:52:20 +0200

    initial commit

---
+++ HOMESYNC_3/HOMESYNC_3/FinishTask.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HOMESYNC_3
+
+{
+    public partial class FinishTask : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void FinishMyTask(object sender, EventArgs e)
+        {
+            try
+            {
+                string connStr = WebConfigurationManager.ConnectionStrings["HOMESYNC_3"].ToString();
+                int user_id1 = Int32.Parse(user_id.Text);
+                string title1 = title.Text;
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    using (SqlCommand FinishMyTaskCommand = new SqlCommand("FinishMyTask", conn))
+                    {
+                        FinishMyTaskCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        FinishMyTaskCommand.Parameters.Add(new SqlParameter("@user_id", user_id1));
+                        FinishMyTaskCommand.Parameters.Add(new SqlParameter("@title", title1));
+                        conn.Open();
+                        object result = FinishMyTaskCommand.ExecuteScalar();                        
+                            outputBox.InnerHtml = $"Task '{title1}' has been finished for user {user_id1}.";
+                        conn.Close();
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                outputBox.InnerHtml = $"SQL Error: {sqlEx.Message}";
+            }
+            catch (Exception ex)
+            {
+                outputBox.InnerHtml = $"Error: {ex.Message}";
+            }
+        }
+
+    }
+}
---
+++ HOMESYNC_3/HOMESYNC_3/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HOMESYNC_3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HOMESYNC_3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d71c7931-e56b-46bd-a819-c64a3beeb36f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
